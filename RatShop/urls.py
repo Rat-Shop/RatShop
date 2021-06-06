4f5e0391a81393b365ec2f2ace8642c214b59c25
Szczurowsky <kamilszczurowski@hotmail.com>
2021-06-06 15:22:11 +0200

    Poprawa routingu

--- RatShop/urls.py
+++ RatShop/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', include('Front'), name="Front"),
+    path('/', include('Front.urls'), name="Front"),
 ]
