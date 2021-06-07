3571b03585257d953d2fb7a712146bf6e1b8ae46
Szczurowsky <kamilszczurowski@hotmail.com>
2021-06-08 00:33:31 +0200

    Added functionality to items

--- categories/views.py
+++ categories/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.conf import settings
 from .models import Category
+from shop.models import Item
 
 
 # Create your views here.
@@ -12,8 +13,11 @@ def generate_category(request, category):
         categories = Category.objects.filter(name=category).first()
     if categories is None:
         return render(request, 'Front/templates/404.html', status=404)
+    items = Item.objects.filter(category=categories).all()
+    print(items)
     context = {
         'shopName': settings.SHOP_NAME,
         'category': categories,
+        'items': items,
     }
     return render(request, 'Front/shop.html', context)
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from categories.models import Category
+
 
 # Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=32)
+    description = models.CharField(max_length=2137)
+    image = models.CharField(max_length=256, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
